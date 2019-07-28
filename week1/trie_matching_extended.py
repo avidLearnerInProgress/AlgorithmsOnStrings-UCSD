0ede0ada8e9bf4d696278a94c091d47faedcaddb
avidLearnerInProgress <chiragshah9696@gmail.com>
2019-07-29 01:27:36 +0530

    Added solutions for week 1

--- week1/trie_matching_extended.py
+++ week1/trie_matching_extended.py
@@ -1,17 +1,49 @@
 # python3
-
 import sys
 
 NA = -1
 
-
 class Node:
+	def __init__ (self):
+		self.next = {}
+		self.isLeaf = False
 
-    def __init__(self):
-        self.next = [NA] * 4
-        self.patternEnd = False
+def solve (text, n, patterns):
+	result = []
+	root = Node()
+	for pattern in patterns:
+		currentNode = root
+		for i, c in enumerate(pattern):
+			if c not in currentNode.next:
+				currentNode.next[c] = Node()
+			if i == len(pattern) - 1:
+				currentNode.next[c].isLeaf = True
+			else:
+				currentNode = currentNode.next[c]
+	
+	for i in range(len(text)):
+		index = i
+		currentNode = root
+		while index < len(text):
+			c = text[index]
+			if c not in currentNode.next: 
+                break
+			currentNode = currentNode.next[c]
+			if currentNode.isLeaf:
+				result.append(i)
+				break
+			index += 1
+	return result
 
+text = sys.stdin.readline ().strip ()
+n = int (sys.stdin.readline ().strip ())
+patterns = []
+for i in range (n):
+	patterns += [sys.stdin.readline ().strip ()]
+ans = solve (text, n, patterns)
+sys.stdout.write (' '.join (map (str, ans)) + '\n')
 
+'''
 def solve(text, n, patterns):
     result = []  #
     trie = []  #
@@ -51,14 +83,4 @@ def solve(text, n, patterns):
             else:
                 break
     return result
-
-
-text = sys.stdin.readline().strip()
-n = int(sys.stdin.readline().strip())
-patterns = []
-for i in range(n):
-    patterns += [sys.stdin.readline().strip()]
-
-ans = solve(text, n, patterns)
-
-sys.stdout.write(' '.join(map(str, ans)) + '\n')
+'''
