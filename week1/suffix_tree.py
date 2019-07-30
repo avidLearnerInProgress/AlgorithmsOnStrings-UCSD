ac340d2ea2619387077bf578820945094e0ab4f4
avidLearnerInProgress <chiragshah9696@gmail.com>
2019-07-31 02:38:01 +0530

    Added solutions for week 1

--- week1/suffix_tree.py
+++ week1/suffix_tree.py
@@ -1,54 +1,117 @@
-# python3
+#python3
+
 import sys
-sys.setrecursionlimit(10000)
+
+class Node:
+    total = 0
+    def __init__(self):
+        Node.total += 1
+        self.id = self.total
+
+class Edge:
+    def __init__(self, startIdx, endIdx, text, startNode, endNode = None):
+        self.startIdx = startIdx
+        self.endIdx = endIdx
+        self.text = text
+        self.startNode = startNode
+        self.endNode = endNode
+
+    def length(self):
+        return self.endIdx - self.startIdx + 1
+    
+    def str(self):
+        return self.text[self.startIdx:self.endIdx+1]
+    
+    def startChar(self):
+        return self.text[self.startIdx]    
+    
+    def __str__(self):
+        return self.startChar()
 
 
-class SuffixTree:
-    class Node:
-        def __init__(self, label):
-            self.label = label
-            self.out = {}
+class SuffixTree: #Naive algorithm
+    def __init__(self, text):
+        self.root = Node()
+        self.text = text
+        self.tree = dict()
+        self.build(self.root, self.text)
+        print('\n'.join(self.exploreEdges(self.tree)))
 
-        def __init__(self, text):
-            self.root = self.Node(None)
-            self.root.out[text[0]] = self.Node(text)
-            for i in range(1, len(text)):
-                current = self.root
-            j = i
-            while j < len(text):
-                if text[j] in current.out:
-                    child = current.out[text[j]]
-                    label = child.label
-                    k = j + 1
-                while k - j < len(label) and text[k] == label[k - j]:
-                    k += 1
-                    if k - j == len(label):
-                        current = child
-                        j = k
-                    else:
-                        cExist, cNew = label[k - j], text[k]
-                        mid = self.Node(label[:k - j])
-                        mid.out[cNew] = self.Node(text[k:])
-                        mid.out[cExist] = child
-                        child.label = label[k - j:]
-                        current.out[text[j]] = mid
+    def match(self, i, root, text):
+        l = len(text)
+        currNode = root
+        atNode = True
+        for j in range(i, l):
+            if atNode:
+                currPos = 0
+                if not text[j] in self.tree[currNode]:
+                    return (currNode, None, j, -1)
                 else:
-                    current.out[text[j]] = self.Node(text[j:])
+                    currEdge = self.tree[currNode][text[j]]
+                    currString = currEdge.str()
+                    lrString = len(currString) - 1
+                    if lrString == 0:
+                        currNode = currEdge.endNode
+                        continue
+                    else:
+                        atNode = False                    
+            else:
+                currPos += 1
+                if text[j] != currString[currPos]:
+                    return (currNode, currEdge, j, currEdge.startIdx + currPos)
+                else:
+                    lrString -= 1
+                    if lrString == 0:
+                        currNode = currEdge.endNode
+                        atNode = True
 
+    
+    def addEdge(self, node, startIdx, endIdx):
+        newEdge = Edge(startIdx, endIdx, self.text, node)
+        self.tree[node][newEdge.startChar()] = newEdge
 
-def build_suffix_tree(text):
-    result = []
-    tree = SuffixTree(text)
-    return tree
+    def splitEdge(self, edge, startIdx, endIdx, cutIdx):
+        newNode = Node()
+        newEdge = Edge(startIdx, endIdx, self.text, newNode)
+        self.tree[newNode] = dict()
+        self.tree[newNode][newEdge.startChar()] = newEdge
+        edge2 = Edge(cutIdx, edge.endIdx, self.text, newNode, edge.endNode)
+        self.tree[newNode][edge2.startChar()] = edge2
+        self.tree[edge.startNode][edge.startChar()].endIdx = cutIdx - 1
+        self.tree[edge.startNode][edge.startChar()].endNode = newNode
 
+    def build(self, root, text):
+        l = len(text)
+        edge1 = Edge(0, l-1, text, root)
+        self.tree[root] = dict()
+        self.tree[root][edge1.startChar()] = edge1
+        #self.printTree(self.tree)
+        for i in range(1, l):
+            currNode, currEdge, j, cutIdx = self.match(i, root, text)
+            #print('####'+str(currNode.id)+','+str(currEdge)+','+str(j)+','+str(cutIdx))
+            if not currEdge:
+                self.addEdge(currNode, j, l-1)
+            else:
+                self.splitEdge(currEdge, j, l-1, cutIdx)
+            #self.printTree(self.tree)
+    
+    def exploreEdges(self, tree):
+        results = []
+        for node in tree.keys():
+            for edge in tree[node].values():
+                results.append(edge.str())
+        return results
+    
+    def printTree(self, tree):
+        for node in tree.keys():
+            print(node.id)
+            for edge in tree[node].values():
+                if edge.endNode == None:
+                    e = None
+                else:
+                    e = edge.endNode.id
+                print(edge.startIdx, edge.endIdx, edge.startNode.id, e, edge.str())
+        print('')
 
-def print_out(node):
-    for child in node.out:
-        print(node.out[child].label)
-        print_out(node.out[child])
-
-
-if __name__ == '__main__':
-    text = sys.stdin.readline().strip()
-    result = build_suffix_tree(text)
-    print_out(result.root)
+if __name__ == "__main__":
+    SuffixTree(input())
