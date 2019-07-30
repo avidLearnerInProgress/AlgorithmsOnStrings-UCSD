cd5f86e30889334b59f4b42c85adb560df4045a5
avidLearnerInProgress <chiragshah9696@gmail.com>
2019-07-31 02:40:21 +0530

    Added solutions for week 1

--- week1/non_shared_substring.py
+++ week1/non_shared_substring.py
@@ -1,52 +1,19 @@
 # python3
 import sys
+import queue
 
+def solve (p, q):
+    l = len(p)
+    m = len(q)
+    for i in range(min(l, m)):
+        for j in range(l-i):
+            s = p[j:j+i+1]
+            if not s in q:
+                return s
+    return
 
-def build_trie(patterns):
-    tree = dict()
-    tree[0] = {}
-    index = 1
+p = sys.stdin.readline ().strip ()
+q = sys.stdin.readline ().strip ()
 
-    for pattern in patterns:
-        current = tree[0]
-        for letter in pattern:
-            if letter in current:
-                current = tree[current[letter]]
-            else:
-                current[letter] = index
-                tree[index] = {}
-                current = tree[index]
-                index = index + 1
-        current['$'] = {}
-    return tree
-
-
-def solve(p, q):
-    patterns = [p, q]
-    for i in range(1, len(p)):
-        patterns.append(p[i:])
-        patterns.append(q[i:])
-
-    trie = build_trie(patterns)
-    print(trie)
-
-    current = trie[0]
-    substring = ""
-
-    while '$' not in current or current == trie[0]:
-        for child in current:
-            substring += child
-            if child not in q:
-                return substring
-            else:
-                current = trie[current[child]]
-                substring = substring[:-1]
-    return p
-
-
-p = sys.stdin.readline().strip()
-q = sys.stdin.readline().strip()
-
-ans = solve(p, q)
-
-sys.stdout.write(ans + '\n')
+ans = solve (p, q)
+sys.stdout.write (ans + '\n')
